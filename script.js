5fc88477c52477dc5cc32fc17629fd748725943e
Chintan Sheth <chintz604@gmail.com>
2020-04-18 21:31:28 +0530

    bug fix

--- script.js
+++ script.js
@@ -27,8 +27,7 @@ map.attributionControl.addAttribution('Population data &copy; <a href="https://e
 // // Basemap layer
 new.L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-});
-.addTo(map);
+}).addTo(map);
 
 // var metalledroads = $getJSON("metalled-roads.geojson", function (data) {
 //   geoJsonLayer = L.geoJson(data, {
