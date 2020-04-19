a7554f7adc1013c31bf8955cfdd06e6b2a07bd38
Chintan Sheth <chintz604@gmail.com>
2020-04-19 23:28:43 +0530

    working map

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var map = L.map('map', {
 
 // layer controls
 var controlLayers = L.control.layers( null, null, {
+      position: "bottomright",
       collapsed: false // false = open by default
     }).addTo(map);
 
