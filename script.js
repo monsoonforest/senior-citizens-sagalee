4480798f661ded4d6efbbd28a053c4ae1e681b65
Chintan Sheth <chintz604@gmail.com>
2020-04-20 02:52:35 +0530

    working map

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var map = L.map('map', {
 
 // layer controls
 var controlLayers = L.control.layers( null, null, {
-     // collapsed: false // false = open by default
+     collapsed: false // false = open by default
     }).addTo(map);
 
 // new L.tileLayer('https://{s}.tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png', {
